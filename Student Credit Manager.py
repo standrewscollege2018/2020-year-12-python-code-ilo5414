baf7a2177b7b4a69832bcfc81d3ba0e42217c460
ilo5414 <ilo5414@stacmail.net>
2020-02-20 19:00:39 +0000

    test

---
+++ Student Credit Manager.py
@@ -0,0 +1,6 @@
+#Student credit manager
+#This programme allows the user to check the total amount of ncea lvl 1 credits.
+
+names = ["Glynn",  "Tash",  "Miles",  "Archie",  "Ben",  "Scott",  "Jay",  "Iona",  "Georgia",  "Charlotte"]
+
+print(names)
