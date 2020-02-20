daa42868b8a979fb4ff40fcfb3232bffe896914f
ilo5414 <ilo5414@stacmail.net>
2020-02-20 19:39:17 +0000

    Created List

---
+++ student credits.py
@@ -0,0 +1,6 @@
+#This program is for a student credit checklist
+
+#student names list
+students = [["Harry",2],  ["Tom", 4], ["Jamie", 6], ["Izzy",8],  ["Max",10],  ["Alexa",12],  ["Georgia",14]]
+
+
