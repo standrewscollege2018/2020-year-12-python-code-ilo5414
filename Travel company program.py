fec27aa2c803d4dfc10646f540eab63aa1bc1719
ilo5414 <ilo5414@stacmail.net>
2020-03-03 12:53:52 +0000

    departure option input added
    with error catching

--- Travel company program.py
+++ Travel company program.py
@@ -13,6 +13,27 @@ print("Lets get your flights sorted!")
 print("Where will you be departing New Zealand from?")
 print("")
 
+#displaying the departure options and the connecting flight costs
 for index in range(0, len(departure)):
     print("{}. {} - connecting flight cost: ${}" .format(index+1, departure[index][0], departure[index][1]))
+
+#getting users selection input
+# in a conditional loop to catch errors both integer and other
+# adding any connecting flight costs to total cost
+ask = True
+while ask == True:
+    try:
+        depart_option = int(input("type here"))
+        #making sure users can only enter a number that represents one of the departure options
+        if depart_option in range(1,4):
+            #if input is valid the loop is set to false and the program carries on. 
+            ask = False
+
+        else:
+            print("Please enter a valid option")
+
+    except:
+        print("Please enter a number") 
+
+
 
