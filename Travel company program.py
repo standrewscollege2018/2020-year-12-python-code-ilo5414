c8972069241f8a72f8958c13937a2617ef622ce1
ilo5414 <ilo5414@stacmail.net>
2020-03-03 12:21:09 +0000

    Lists and constant created

---
+++ Travel company program.py
@@ -0,0 +1,12 @@
+#travel company program
+#this program will calculate the total cost of a customers holiday from flights to accomodation costs
+
+departure = [["Auckland", 0], ["Wellington", 50], ["Christchurch", 75]]
+destination = [["Sydney", 326, 120], ["Tonga", 378, 40], ["Shanghai", 1436, 60], ["London", 2376, 80]]
+total_cost = 0
+print(departure)
+print(destination)
+
+print("Welcome to Logie's Legendary travel service!")
+print("We are so happy we can help you today")
+
