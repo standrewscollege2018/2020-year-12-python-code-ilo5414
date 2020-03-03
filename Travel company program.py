415f8d7d82f2c6a8f4b3ee50f570890cc0b70f4d
ilo5414 <ilo5414@stacmail.net>
2020-03-03 12:28:24 +0000

    displaying the departure options
    using a for loop

--- Travel company program.py
+++ Travel company program.py
@@ -4,9 +4,15 @@
 departure = [["Auckland", 0], ["Wellington", 50], ["Christchurch", 75]]
 destination = [["Sydney", 326, 120], ["Tonga", 378, 40], ["Shanghai", 1436, 60], ["London", 2376, 80]]
 total_cost = 0
-print(departure)
-print(destination)
 
 print("Welcome to Logie's Legendary travel service!")
 print("We are so happy we can help you today")
+print("")
+
+print("Lets get your flights sorted!")
+print("Where will you be departing New Zealand from?")
+print("")
+
+for index in range(0, len(departure)):
+    print("{}. {} - connecting flight cost: ${}" .format(index+1, departure[index][0], departure[index][1]))
 
