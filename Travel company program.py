651070e65f02722851724cffd6c3320e32a7846b
ilo5414 <ilo5414@stacmail.net>
2020-03-03 13:13:42 +0000

    added some commenting and displayed destination options

--- Travel company program.py
+++ Travel company program.py
@@ -29,17 +29,35 @@ while ask == True:
         if depart_option in range(1,4):
             #if input is valid the loop is set to false and the program carries on. 
             ask = False
-
+            
+        #catching inputs that are not 1 2 or 3 
         else:
             print("Please enter a valid option")
-
+            
+    #catching inputs that are not integer
     except:
         print("Please enter a number")
 
-    trip.append(departure[depart_option-1])
-    total_cost = trip[0][1]
-    print("Total cost of your trip so far..",  total_cost)
-    print("Looks like you are departing from..")
-    print(trip[0][0])
+#appending the departure the user chose to the trip list
+trip.append(departure[depart_option-1])
+#adding connecting flight costs to the total cost
+total_cost = trip[0][1]
+#displaying the total cost
+print("Total cost of your trip so far..",  total_cost)
+#displaying where they are departing from
+print("Looks like you are departing from..")
+print(trip[0][0])
+print("")
+
+#destination section
+print("Time to select your destination!")
+print("please select one of the following destinations")
+print("")
+#displaying the destination options and flight costs 
+for index in range(0, len(destination)):
+    print("{}. {} - connecting flight cost: ${}" .format(index+1, destination[index][0], destination[index][1]))
+
+
+    
 
 
