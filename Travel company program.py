c36677a3e14a66b9bcaf79dc35680333a7ead2a0
ilo5414 <ilo5414@stacmail.net>
2020-03-04 09:21:20 +0000

    destination option input and calculations to total cost adding destination added

--- Travel company program.py
+++ Travel company program.py
@@ -43,12 +43,14 @@ trip.append(departure[depart_option-1])
 #adding connecting flight costs to the total cost
 total_cost = trip[0][1]
 #displaying the total cost
-print("Total cost of your trip so far..",  total_cost)
+print("Total cost of your trip so far..$",total_cost)
 #displaying where they are departing from
 print("Looks like you are departing from..")
 print(trip[0][0])
 print("")
 
+
+##########################################################################
 #destination section
 print("Time to select your destination!")
 print("please select one of the following destinations")
@@ -58,6 +60,47 @@ for index in range(0, len(destination)):
     print("{}. {} - connecting flight cost: ${}" .format(index+1, destination[index][0], destination[index][1]))
 
 
-    
+ask = True
+while ask == True:
+    try:
+        destination_option = int(input("type here"))
+        #making sure users can only enter a number that represents one of the destination options
+        if destination_option in range(1,4):
+            #if input is valid the loop is set to false and the program carries on. 
+            ask = False
+            
+        #catching inputs that are not 1 2 or 3 
+        else:
+            print("Please enter a valid option")
+            
+    #catching inputs that are not integer
+    except:
+        print("Please enter a number")
+
+#appending the destination to the trip list   
+trip.append(destination[destination_option-1])
+#calculating the new total cost
+total_cost = total_cost + destination[destination_option-1][1]
+
+#displaying the total cost
+print("Total cost of your trip so far..$",total_cost)
+
+#displaying the destination the user has chosen
+print("Looks like you are heading to...")
+print(trip[1][0])
+print("")
+
+
+###################################################################
+#accomodation section
+
+
+
+
+
+
+
+
+
 
 
